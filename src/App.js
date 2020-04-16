e261470e28c12399046f023e1efc75e20c888a27
Gustavo <gcrodrigues413@gmail.com>
2020-04-16 06:49:03 -0300

    Criação de métodos import dos componentes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import Tabela from "./Tabela";
+import Form from "./Formulario";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    autores: [
+      {
+        nome: "Paulo",
+        livro: "React",
+        preco: "1000",
+      },
+      {
+        nome: "Daniel",
+        livro: "Java",
+        preco: "99",
+      },
+      {
+        nome: "Marcos",
+        livro: "Design",
+        preco: "150",
+      },
+      {
+        nome: "Bruno",
+        livro: "DevOps",
+        preco: "100",
+      },
+    ],
+  };
+
+  removeAutor = (index) => {
+    const { autores } = this.state;
+
+    this.setState({
+      autores: autores.filter((autor, posAtual) => {
+        return posAtual !== index;
+      }),
+    });
+  };
+
+  escutadorDeSubmit = (autor) => {
+    this.setState({
+      autores: [...this.state.autores, autor],
+    });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
+        <Form escutadorDeSubmit={this.escutadorDeSubmit} />
+      </Fragment>
+    );
+  }
 }
-
-export default App;
